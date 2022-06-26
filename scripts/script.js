566899528e3e6ad85300a7f0a2c0a7efb5c8c54d
Anna Groshkova <anna.matvyeyenko@gmail.com>
2022-06-26 20:58:01 +0200

    second review fixes

--- scripts/script.js
+++ scripts/script.js
@@ -11,8 +11,8 @@ let jobInput = document.querySelector('.popup__input_type_description');
 function openPopup() {
   popup.classList.add('popup_opened');
 
-  profileName.textContent = nameInput.value;
-  profileJob.textContent = jobInput.value;
+  nameInput.value=  profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
 
 function closePopup() {
